3d3bff1252ffcc2fe06267a42e261ed7d80975b1
rayanate <rayanate@mail.uc.edu>
2024-03-15 17:35:18 -0400

    Initial setup

---
+++ src/main/java/com/libraryDashboard/libraryDashboard/LibraryDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.libraryDashboard.libraryDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libraryDashboard/libraryDashboard/LibraryDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryDashboard.libraryDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
